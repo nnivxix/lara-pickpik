50f0e318180bfe97be1aa5797ccddf65218cd994
nnivxix <adhe.hanasa@gmail.com>
2025-03-24 13:14:32 +0700

    feat: add PhotoSearchController for searching photos by description

---
+++ app/Http/Controllers/PhotoSearchController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Photo;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PhotoResource;
+
+class PhotoSearchController extends Controller
+{
+
+    public function __invoke(Request $request)
+    {
+        try {
+            $request->validate([
+                'query' => 'required|string|min:3',
+            ]);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json([
+                'message' => 'The given data was invalid.',
+                'errors' => $e->errors(),
+            ], 422);
+        }
+
+        $query = $request->query('query');
+
+        $photos = Photo::query()
+            ->where('description', 'like', "%{$query}%")
+            ->orWhere('alt_description', 'like', "%{$query}%")
+            ->limit(10)
+            ->get();
+
+        $collection = PhotoResource::collection($photos);
+        $collection->wrap('results');
+
+        return $collection->additional([
+            // dummy data for total and total_pages
+            'total' => 1000,
+            'total_pages' => 100,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\PhotoRandomController;
+use App\Http\Controllers\PhotoSearchController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,4 @@ Route::get('/photos', [PhotoController::class, 'index'])->name('api.photos.index
 Route::get('/photos/{photo}', [PhotoController::class, 'show'])->name('api.photos.show');
 
 Route::get('/random/photo', PhotoRandomController::class)->name('api.photos.random');
+Route::get('/search/photos', PhotoSearchController::class)->name('api.photos.search');
